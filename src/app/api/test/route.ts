e2a2eb8e84e56bb78ef805171c8a3530d5663586
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-03-15 18:36:36 -0300

    removing localhost

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,10 +1,10 @@
 export async function POST(req: Request) {
-   return new Response(await fetch("http://localhost:3000/api/upload", {
+   return new Response(await fetch(`${process.env.VERCEL_URL}/api/upload`, {
       method: "POST",
       body: await req.arrayBuffer() 
     })
 
-      .then(async res => fetch("http://localhost:3000/api/decrypt", {
+      .then(async res => fetch(`${process.env.VERCEL_URL}/api/decrypt`, {
         method: "POST",
         body: await res.arrayBuffer()
       })
