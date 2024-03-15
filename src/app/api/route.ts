7da674ea66f3fc10ca347d821db854d1d4f3b526
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-03-15 18:29:34 -0300

    Adjustment

--- src/app/api/route.ts
+++
@@ -1,6 +0,0 @@
-import {createReadStream} from "fs"
-import {} from "form-data"
-import path from "path"
-export async function POST(){
-  const file = createReadStream(path.resolve(__dirname, "..", "..", ))
-}
